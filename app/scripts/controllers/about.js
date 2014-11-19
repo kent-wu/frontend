eba06db5702b4d20f354d7539e7bebc4d39cfde9
Kent WU <twer@Kent-Wu-2.local>
2014-11-19 16:53:49 +0800

    kent - init kehutong_frontend

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kehutongFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kehutongFrontendApp
+ */
+angular.module('kehutongFrontendApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kehutongFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kehutongFrontendApp
+ */
+angular.module('kehutongFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
